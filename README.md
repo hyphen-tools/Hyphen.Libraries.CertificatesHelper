0951b1b1572765dc5ff21d7109489f0791779152
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2022-05-26 15:54:54 +0100

    feat: certificates project

--- README.md
+++ README.md
@@ -3,17 +3,17 @@
 </div>
 <br>
 <p align="middle">
-  <img src="https://github.com/makehyphen/Hyphen.Templates.GithubPackage/actions/workflows/dotnet.yml/badge.svg"/>
+  <img src="https://github.com/makehyphen/Hyphen.Libraries.CertificatesHelper/actions/workflows/dotnet.yml/badge.svg"/>
 </p>
 
 <h3 align="middle">
-   Hyphen.Templates.GithubPackage
+   Hyphen.Libraries.CertificatesHelper
 </h3>
 <p align="middle">Description</p>
 
 
 <p align="middle">
-  <sup><a href="https://hyphen.so"><b>Hyphen</b></a> � <b>Hyphen.Templates.GithubPackage</b> released under <a href="LICENSE.md">MIT</a> license, created by <a href="https://github.com/emimontesdeoca">Emiliano Montesdeoca</a>.
+  <sup><a href="https://hyphen.so"><b>Hyphen</b></a> � <b>Hyphen.Libraries.CertificatesHelper</b> released under <a href="LICENSE.md">MIT</a> license, created by <a href="https://github.com/emimontesdeoca">Emiliano Montesdeoca</a>.
     </sup></p>
 
 
---
+++ X509CertificatesHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyphen.Libraries.CertificatesHelper
+{
+    public class X509CertificatesHelpers
+    {
+        /// <summary>
+        /// Gets the certificate from a certain url
+        /// </summary>
+        /// <param name="urlPath">Url path</param>
+        /// <returns>X509Certificate2 object</returns>
+        static async Task<X509Certificate2> GetX509CertificateAsync(string urlPath)
+        {
+            var certificate = new X509Certificate2();
+
+            var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (request, cert, chain, policyErrors) =>
+                {
+                    certificate = new X509Certificate2(cert);
+                    return true;
+                }
+            };
+
+            using HttpClient httpClient = new HttpClient(httpClientHandler);
+            await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, urlPath));
+
+            return certificate;
+        }
+    }
+}
